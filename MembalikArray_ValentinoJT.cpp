262b4fe8d55cf69db00e6b5c287be36928407e6f
ValentinoJT <89502769+ValentinoJT@users.noreply.github.com>
2024-12-09 15:23:29 +0700

    Add files via upload

---
+++ MembalikArray_ValentinoJT.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int myNumbers[5] = {5, 10, 15, 20, 25};
+    for (int i = 4; i >= 0; i--) {
+        cout << myNumbers[i] << " ";
+    }
+
+return 0;
+}
