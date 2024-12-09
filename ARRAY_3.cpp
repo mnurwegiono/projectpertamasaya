3305eec3d4c84f9d87814e64c9598edab5fafa88
chelle <chanangelchan28@gmail.com>
2024-12-09 15:19:37 +0700

    Add files via upload
    Mmebalik Urutan Elemen Array

---
+++ ARRAY_3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int size = 5; 
+
+    for (int i = 0; i < size / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[size - 1 - i];
+        arr[size - 1 - i] = temp;
+    }
+    
+    cout << "Array setelah dibalik: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
