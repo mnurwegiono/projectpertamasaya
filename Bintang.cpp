9b471174e4e1af74b0ccbae8546da84244cfdde1
han <bintangyodisa25@gmail.com>
2024-12-09 21:31:58 +0700

    Update Bintang.cpp

--- Bintang.cpp
+++ Bintang.cpp
@@ -2,18 +2,16 @@
 using namespace std;
 
 int main (){
-
     int arr[]{1,2,3,4,5};
-
     cout << "Array setelah dibalik: [";
     
     for (int i=4;i>=0;i--){
-        
-        if (i>0){
-            cout << arr[i] << ",";
-        } else {cout << arr[i] << "]";}
+        cout << arr[i];
 
+        if (i > 0){
+            cout << ",";
+        }
     }
-
+    cout << "]";
     return 0;
 }
