1bea254de3d3503c8e2c9c2fe43b92fbb5bbbfee
han <bintangyodisa25@gmail.com>
2024-12-10 20:37:28 +0700

    Update Bintang.cpp

--- Bintang.cpp
+++ Bintang.cpp
@@ -4,7 +4,42 @@ using namespace std;
 int main (){
     int arr[]{1,2,3,4,5};
     cout << "Array setelah dibalik: [";
-    
+    #include <iostream>
+using namespace std;
+
+int main (){
+
+    const int JUMLAH_ELEMENT{5};
+    int arr[]{1,2,3,4,5};
+
+    int simpan_value;
+
+    // menukar posisi indeks 0 dengan 4, dan 1 dengan 3;
+    for (int i = 0; i < 2; i++){
+
+        simpan_value = arr[i]; 
+        arr[i] = arr [JUMLAH_ELEMENT-i-1]; 
+        arr [JUMLAH_ELEMENT-i-1] = simpan_value;
+
+    }
+
+    // menampilkan array
+    cout << "Array setelah dibalik: [";
+
+    for (int i=0;i<5;i++){
+
+        cout << arr[i];
+        if (i != 4){
+            cout << ",";
+        } else {
+            cout << "]";
+        }
+
+    }
+
+    return 0;
+
+}
     for (int i=4;i>=0;i--){
         cout << arr[i];
 
